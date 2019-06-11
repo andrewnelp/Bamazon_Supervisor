19da44b92c882bd6a197c17c20abf8bf8adaabde
Andrey Nelepov <nelepow@gmail.com>
2019-06-11 12:36:53 -0400

    appp is finished and working

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -39,32 +39,56 @@ const runSearch = () => {
           break;
 
         case "Create New Department":
-          // createDep();
+          createDep();
           break;
       }
     });
 }
 
-const viewProductsByDep = () =>{
+const viewProductsByDep = () => {
   let query = "SELECT departments.department_id, departments.department_name, departments.over_head_costs, products.product_sales ";
   query += "FROM departments INNER JOIN products ON (departments.department_name = products.department_name) ";
   query += "ORDER BY products.department_name, products.product_sales DESC";
-  connection.query(query, (err, res) =>{
-    if(err) throw err;
-    console.log(`=====================================================================`)
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.log(`===============================================================================`)
     console.log(`Department ID || Department Name  || Overhead Costs || Product Sales || Profit`);
-    
+    console.log(`...............................................................................`)
+
     res.forEach(r => {
       let profit = r.product_sales - r.over_head_costs;
       console.log(`    ${r.department_id}         ||      ${r.department_name}     ||      ${r.over_head_costs}       ||${r.product_sales}          ||  ${profit}`);
     })
-    console.log(`=====================================================================`)
-    // connection.end();
+    console.log(`===============================================================================`)
+    connection.end();
     runSearch();
 
   })
 };
 
-// const createDep = () => {
-  
-// }
+const createDep = () => {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "What is department name?"
+      },
+      {
+        name: "costs",
+        type: "input",
+        message: "What are over head costs?"
+      },
+    ])
+    .then(answer => {
+      let query = "INSERT INTO departments(department_name, over_head_costs) VALUES (?, ?)";
+      connection.query(query, [answer.name, answer.costs], (err, res) => {
+        if (err) throw err;
+        console.log('===============================================================================================');
+        console.log(res.affectedRows + " department added!\n");
+        console.log('===============================================================================================');
+        runSearch();
+
+      })
+    })
+}
